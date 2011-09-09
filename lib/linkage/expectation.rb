21f8d795e97fe76c14bacd5fc25bae31e113791c
Jeremy Stephens <viking415@gmail.com>
2011-09-09 15:32:33 -0500

    Add Expectation#kind

--- lib/linkage/expectation.rb
+++ lib/linkage/expectation.rb
@@ -5,10 +5,18 @@ module Linkage
     end
 
     attr_reader :operator, :field_1, :field_2
+
     def initialize(operator, field_1, field_2)
       @operator = operator
       @field_1 = field_1
       @field_2 = field_2
+    end
+
+    # @return [Symbol] :join or :filter
+    def kind
+      if @field_1.dataset != @field_2.dataset
+        :join
+      end
     end
   end
 
--- test/unit/test_expectation.rb
+++ test/unit/test_expectation.rb
@@ -8,4 +8,14 @@ class UnitTests::TestExpectation < Test::Unit::TestCase
   test "get" do
     assert_equal Linkage::MustExpectation, Linkage::Expectation.get(:must)
   end
+
+  test "kind when two fields from different datasets" do
+    dataset_1 = mock('dataset 1')
+    dataset_2 = mock('dataset 2')
+    dataset_1.expects(:==).with(dataset_2).returns(false)
+    field_1 = mock('field 1', :dataset => dataset_1)
+    field_2 = mock('field 2', :dataset => dataset_2)
+    exp = Linkage::MustExpectation.new(:==, field_1, field_2)
+    assert_equal :join, exp.kind
+  end
 end
