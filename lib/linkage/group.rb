de537f941d7786907ce3ff6aefb8e61a24a19460
Jeremy Stephens <viking415@gmail.com>
2011-09-12 16:21:40 -0500

    Create Linkage::Group

---
+++ lib/linkage/group.rb
@@ -0,0 +1,35 @@
+module Linkage
+  # This class represents a group of records that match based on criteria
+  # described via the {Dataset#link_with} method. Group's are created by
+  # subclasses of the {Runner} class during execution.
+  #
+  # @see Dataset#link_with
+  # @see SingleThreadedRunner
+  class Group
+    # @return [Array<Object>] An array of this group's record ids
+    attr_reader :records
+
+    # @param [Hash] matching_values Values that define this group
+    # @example
+    #   Linkage::Group.new({:foo => 123, :bar => 'baz'})
+    def initialize(matching_values)
+      @values = matching_values
+      @records = []
+    end
+
+    # Check to see if the given set of values matches this group's values.
+    #
+    # @param [Hash] values Hash of values
+    # @return [Boolean] true if match, false if not
+    def matches?(values)
+      @values == values
+    end
+
+    # Add a record id to this group's set of records.
+    #
+    # @param [Object] record_id
+    def add_record(record_id)
+      @records << record_id
+    end
+  end
+end
---
+++ test/unit/test_group.rb
@@ -0,0 +1,15 @@
+require 'helper'
+
+class UnitTests::TestGroup < Test::Unit::TestCase
+  test "matches?" do
+    g = Linkage::Group.new(:test => 'test')
+    assert g.matches?({:test => 'test'})
+    assert !g.matches?({:foo => 'bar'})
+  end
+
+  test "add_record adds a record" do
+    g = Linkage::Group.new(:test => 'test')
+    g.add_record(123)
+    assert_equal [123], g.records
+  end
+end
