f9dc1633478051cc9e00b004048fe970e5910f70
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2013-11-14 15:03:41 -0600

    Explicitly use Sequel core extensions

--- lib/linkage.rb
+++ lib/linkage.rb
@@ -25,6 +25,7 @@ require path + 'result_set'
 require path + 'field_set'
 require path + 'comparator'
 
+Sequel.extension :core_extensions
 Sequel.extension :collation
 if Sequel::Collation.respond_to?(:suppress_warnings=)
   Sequel::Collation.suppress_warnings = true
