0a00ec36c21ed9f7d675963edf823612c37516f0
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2013-12-05 11:13:41 -0600

    Remove parameter handling in Comparator

--- lib/linkage/comparator.rb
+++ lib/linkage/comparator.rb
@@ -16,16 +16,8 @@ module Linkage
         raise ArgumentError, "class must define the score method"
       end
 
-      begin
-        if klass.parameters.length > 0
-          @comparators ||= {}
-          @comparators[name] = klass
-        else
-          raise ArgumentError, "class must have at least one parameter"
-        end
-      rescue NotImplementedError
-        raise ArgumentError, "parameters class method must be defined"
-      end
+      @comparators ||= {}
+      @comparators[name] = klass
     end
 
     def self.[](name)
@@ -38,113 +30,11 @@ module Linkage
       raise NotImplementedError
     end
 
-    # @abstract Override this to require a specific number of arguments of a
-    #   certain class. To require two parameters of either String or Integer,
-    #   do something like this:
-    #
-    #     @@parameters = [[String, Integer], [String, Integer]]
-    #     def self.parameters
-    #       @@parameters
-    #     end
-    #
-    #   At least one argument must be defined.
-    # @return [Array]
-    def self.parameters
-      raise NotImplementedError
-    end
-
-    attr_reader :args, :lhs_args, :rhs_args
-
-    # Create a new Comparator object.
-    # @param [Linkage::MetaObject, Hash] args Comparator arguments
-    def initialize(*args)
-      @args = args
-      @lhs_args = []
-      @rhs_args = []
-      @options = args.last.is_a?(Hash) ? args.pop : {}
-      process_args
-    end
-
     # @abstract Override this to return the score of the linkage strength of
     #   two records.
     # @return [Numeric]
     def score(record_1, record_2)
       raise NotImplementedError
-    end
-
-    private
-
-    def process_args
-      parameters = self.class.parameters
-      if parameters.length != @args.length
-        raise ArgumentError, "wrong number of arguments (#{@args.length} for #{parameters.length})"
-      end
-
-      first_side = nil
-      second_side = nil
-      @args.each_with_index do |arg, i|
-        type = arg.ruby_type[:type]
-
-        parameter_types = parameters[i]
-        if parameter_types.last.is_a?(Hash)
-          parameter_options = parameter_types[-1]
-          parameter_types = parameter_types[0..-2]
-        else
-          parameter_options = {}
-        end
-
-        if parameter_types[0] != :any && !parameter_types.include?(type)
-          raise TypeError, "expected type #{parameters[i].join(" or ")}, got #{type}"
-        end
-
-        if parameter_options.has_key?(:values) && arg.raw? && !parameter_options[:values].include?(arg.object)
-          raise ArgumentError, "argument #{i + 1} (#{arg.object.inspect}) was not one of the expected values: #{parameter_options[:values].inspect}"
-        end
-
-        if parameter_options.has_key?(:same_type_as)
-          arg_index = parameter_options[:same_type_as]
-          other_type = @args[arg_index].ruby_type[:type]
-          if type != other_type
-            raise TypeError, "argument #{i + 1} (#{type}) was expected to have the same type as argument #{arg_index + 1} (#{other_type})"
-          end
-        end
-
-        if parameter_options.has_key?(:static) &&
-              parameter_options[:static] != arg.static?
-          raise TypeError, "argument #{i + 1} was expected to #{arg.static? ? "not be" : "be"} static"
-        end
-
-        if !arg.static?
-          if first_side.nil?
-            first_side = arg.side
-          elsif arg.side != first_side && second_side.nil?
-            second_side = arg.side
-          end
-
-          valid_side = true
-          case parameter_options[:side]
-          when :first
-            if arg.side != first_side
-              valid_side = false
-            end
-          when :second
-            if second_side.nil? || arg.side != second_side
-              valid_side = false
-            end
-          end
-
-          if !valid_side
-            raise TypeError, "argument #{i + 1} was expected to have a different side value"
-          end
-
-          case arg.side
-          when :lhs
-            @lhs_args << arg
-          when :rhs
-            @rhs_args << arg
-          end
-        end
-      end
     end
   end
 end
--- test/unit/test_comparator.rb
+++ test/unit/test_comparator.rb
@@ -26,89 +26,10 @@ class UnitTests::TestComparator < Test::Unit::TestCase
     assert_equal klass, Linkage::Comparator['foo']
   end
 
-  test "parameters raises error in base class" do
-    assert_raises(NotImplementedError) { Linkage::Comparator.parameters }
-  end
-
-  test "subclasses required to define parameters class method" do
-    klass = new_comparator('foo')
-    assert_raises(ArgumentError) { Linkage::Comparator.register(klass) }
-  end
-
-  test "subclasses required to define at least one parameter" do
-    klass = new_comparator('foo', [])
-    assert_raises(ArgumentError) { Linkage::Comparator.register(klass) }
-  end
-
   test "subclasses required to define score method" do
     klass = new_comparator('foo', [[String]]) do
       remove_method :score
     end
     assert_raises(ArgumentError) { Linkage::Comparator.register(klass) }
-  end
-
-  test "comparator with one valid argument" do
-    klass = new_comparator('foo', [[String]])
-    meta_object = stub('meta object', :side => :lhs, :ruby_type => { :type => String }, :static? => false)
-    f = klass.new(meta_object)
-  end
-
-  test "comparator with one invalid argument" do
-    klass = new_comparator('foo', [[String]])
-    meta_object = stub('meta_object', :ruby_type => { :type => Fixnum }, :static? => false)
-    assert_raises(TypeError) { klass.new(meta_object) }
-  end
-
-  test "comparator with too few arguments" do
-    klass = new_comparator('foo', [[String]])
-    assert_raises(ArgumentError) { klass.new }
-  end
-
-  test "comparator with too many arguments" do
-    klass = new_comparator('foo', [[String]])
-    meta_object = stub('meta_object', :ruby_type => { :type => String }, :static? => false)
-    assert_raises(ArgumentError) { klass.new(meta_object, meta_object) }
-  end
-
-  test "requiring argument to be non-static" do
-    klass = new_comparator('foo', [[String, :static => false]])
-    meta_object = stub('meta_object', :ruby_type => { :type => String }, :static? => true)
-    assert_raise_message("argument 1 was expected to not be static") do
-      klass.new(meta_object)
-    end
-  end
-
-  test "requiring argument to be static" do
-    klass = new_comparator('foo', [[String, :static => true]])
-    meta_object = stub('meta_object', :ruby_type => { :type => String }, :static? => false)
-    assert_raise_message("argument 1 was expected to be static") do
-      klass.new(meta_object)
-    end
-  end
-
-  test "special :any parameter" do
-    klass = new_comparator('foo', [[:any]])
-    meta_object_1 = stub('meta_object', :side => :lhs, :ruby_type => { :type => String }, :static? => false)
-    meta_object_2 = stub('meta_object', :side => :rhs, :ruby_type => { :type => Fixnum }, :static? => false)
-    assert_nothing_raised do
-      klass.new(meta_object_1)
-      klass.new(meta_object_2)
-    end
-  end
-
-  test "lhs_args" do
-    klass = new_comparator('foo', [[String], [String]])
-    meta_object_1 = stub('meta object 1', :side => :lhs, :ruby_type => { :type => String }, :static? => false)
-    meta_object_2 = stub('meta object 2', :side => :rhs, :ruby_type => { :type => String }, :static? => false)
-    obj = klass.new(meta_object_1, meta_object_2)
-    assert_equal [meta_object_1], obj.lhs_args
-  end
-
-  test "rhs_args" do
-    klass = new_comparator('foo', [[String], [String]])
-    meta_object_1 = stub('meta object 1', :side => :lhs, :ruby_type => { :type => String }, :static? => false)
-    meta_object_2 = stub('meta object 2', :side => :rhs, :ruby_type => { :type => String }, :static? => false)
-    obj = klass.new(meta_object_1, meta_object_2)
-    assert_equal [meta_object_2], obj.rhs_args
   end
 end
