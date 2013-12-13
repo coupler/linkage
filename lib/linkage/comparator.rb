74192ca4d893acc5080cf0df4f4fde5081a5ba2c
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2013-12-13 11:19:51 -0600

    Remove Comparator#score_datasets implementation
    - the default implementation should be handled in the runner for the
      sake of speed
    - added Comparator#type
    - require subclasses of Comparator to define both score_datasets and
      score_dataset if they don't define the score method

--- lib/linkage/comparator.rb
+++ lib/linkage/comparator.rb
@@ -8,8 +8,8 @@ module Linkage
     # @param [Class] klass Comparator subclass
     def self.register(name, klass)
       methods = klass.instance_methods(false)
-      if !methods.include?(:score) && !methods.include?(:score_datasets)
-        raise ArgumentError, "class must define either #score or #score_datasets"
+      if !methods.include?(:score) && (!methods.include?(:score_datasets) || !methods.include?(:score_dataset))
+        raise ArgumentError, "class must define either #score or both #score_datasets and #score_dataset methods"
       end
 
       @comparators ||= {}
@@ -26,41 +26,15 @@ module Linkage
       raise NotImplementedError
     end
 
+    def type
+      @type || :simple
+    end
+
     # @abstract Override this to return the score of the linkage strength of
     #   two records.
     # @return [Numeric]
     def score(record_1, record_2)
       raise NotImplementedError
-    end
-
-    # Default algorithm for scoring datasets. Calls the score method for each
-    # pair of records.
-    def score_datasets(dataset_1, dataset_2 = nil)
-      if dataset_2
-        dataset_1.each do |record_1|
-          dataset_2.each do |record_2|
-            score_and_notify(record_1, record_2)
-          end
-        end
-      else
-        # very naive implementation
-        records = dataset_1.all
-        0.upto(records.length - 2) do |i|
-          record_1 = records[i]
-          (i + 1).upto(records.length - 1) do |j|
-            record_2 = records[j]
-            score_and_notify(record_1, record_2)
-          end
-        end
-      end
-    end
-
-    protected
-
-    def score_and_notify(record_1, record_2)
-      value = score(record_1, record_2)
-      changed
-      notify_observers(record_1, record_2, value)
     end
   end
 end
--- test/unit/test_comparator.rb
+++ test/unit/test_comparator.rb
@@ -11,78 +11,43 @@ class UnitTests::TestComparator < Test::Unit::TestCase
     super
   end
 
+  test "default subclass has type of simple" do
+    klass = new_comparator
+    instance = klass.new
+    assert_equal :simple, instance.type
+  end
+
   test "getting a registered subclass" do
     klass = new_comparator
     Linkage::Comparator.register('foo', klass)
     assert_equal klass, Linkage::Comparator['foo']
   end
 
-  test "registering subclass with score_datasets method instead of score method" do
+  test "registering subclass with advanced scoring methods" do
     klass = new_comparator do
       remove_method :score
-      def score_datasets(dataset_1, dataset_2 = nil)
+
+      def score_datasets(dataset_1, dataset_2)
+      end
+
+      def score_dataset(dataset)
       end
     end
     assert_nothing_raised { Linkage::Comparator.register('foo', klass) }
   end
 
-  test "subclasses required to define score method OR score_datasets method" do
+  test "subclasses required to define either simple or advanced scoring methods" do
     klass = new_comparator do
       remove_method :score
     end
     assert_raises(ArgumentError) { Linkage::Comparator.register('foo', klass) }
-  end
 
-  test "default score_datasets implementation for two datasets" do
-    klass = new_comparator
-    instance = klass.new
-    observer = stub('observer', :update => nil)
-    instance.add_observer(observer)
+    klass = new_comparator do
+      remove_method :score
 
-    dataset_1 = stub('dataset 1')
-    dataset_1.expects(:each).multiple_yields(
-      [(record_1_1 = {:id => 1, :foo => 123})],
-      [(record_1_2 = {:id => 2, :foo => 456})]
-    )
-
-    dataset_2 = stub('dataset 2')
-    dataset_2.expects(:each).twice.multiple_yields(
-      [(record_2_1 = {:id => 100, :foo => 123})],
-      [(record_2_2 = {:id => 101, :foo => 456})]
-    )
-
-    instance.expects(:score).with(record_1_1, record_2_1).returns(1)
-    observer.expects(:update).with(record_1_1, record_2_1, 1)
-    instance.expects(:score).with(record_1_1, record_2_2).returns(0)
-    observer.expects(:update).with(record_1_1, record_2_2, 0)
-    instance.expects(:score).with(record_1_2, record_2_1).returns(0)
-    observer.expects(:update).with(record_1_2, record_2_1, 0)
-    instance.expects(:score).with(record_1_2, record_2_2).returns(1)
-    observer.expects(:update).with(record_1_2, record_2_2, 1)
-
-    instance.score_datasets(dataset_1, dataset_2)
-  end
-
-  test "default score_datasets implementation for one dataset" do
-    klass = new_comparator
-    instance = klass.new
-    observer = stub('observer', :update => nil)
-    instance.add_observer(observer)
-
-    dataset = stub('dataset')
-    dataset.expects(:all).returns([
-      (record_1 = {:id => 1, :foo => 123}),
-      (record_2 = {:id => 2, :foo => 456}),
-      (record_3 = {:id => 3, :foo => 456})
-    ])
-
-    instance.expects(:score).with(record_1, record_2).returns(0)
-    observer.expects(:update).with(record_1, record_2, 0)
-    instance.expects(:score).with(record_1, record_3).returns(0)
-    observer.expects(:update).with(record_1, record_3, 0)
-    instance.expects(:score).with(record_2, record_3).returns(1)
-    observer.expects(:update).with(record_2, record_3, 1)
-
-    instance.score_datasets(dataset)
+      def score_datasets(dataset_1, dataset_2)
+      end
+    end
+    assert_raises(ArgumentError) { Linkage::Comparator.register('foo', klass) }
   end
 end
