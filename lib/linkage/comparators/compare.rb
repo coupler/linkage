581e7ba88fe69a7b21709cfef578d29a1b4f4279
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2013-12-18 16:43:39 -0600

    Add :equal_to operator to Compare
    - using :equal_to in Compare switches to advanced comparator mode
    - use to_expr in Compare#score instead of name
    - implement sorted comparisons in Compare#score_datasets and
      Compare#score_dataset for :equal_to

--- lib/linkage/comparators/compare.rb
+++ lib/linkage/comparators/compare.rb
@@ -3,7 +3,7 @@ module Linkage
     class Compare < Comparator
       VALID_OPERATIONS = [
         :not_equal, :greater_than, :greater_than_or_equal_to,
-        :less_than_or_equal_to, :less_than
+        :less_than_or_equal_to, :less_than, :equal_to
       ]
 
       def initialize(set_1, set_2, operation)
@@ -23,6 +23,7 @@ module Linkage
         if !VALID_OPERATIONS.include?(operation)
           raise "operation is not valid"
         end
+        @type = operation == :equal_to ? :advanced : :simple
 
         @set_1 = set_1
         @set_2 = set_2
@@ -30,24 +31,169 @@ module Linkage
       end
 
       def score(record_1, record_2)
-        name_1 = @set_1[0].name
-        name_2 = @set_2[0].name
+        expr_1 = @set_1[0].to_expr
+        expr_2 = @set_2[0].to_expr
 
         result =
           case @operation
           when :not_equal
-            record_1[name_1] != record_2[name_2]
+            record_1[expr_1] != record_2[expr_2]
           when :greater_than
-            record_1[name_1] > record_2[name_2]
+            record_1[expr_1] > record_2[expr_2]
           when :greater_than_or_equal_to
-            record_1[name_1] >= record_2[name_2]
+            record_1[expr_1] >= record_2[expr_2]
           when :less_than_or_equal_to
-            record_1[name_1] <= record_2[name_2]
+            record_1[expr_1] <= record_2[expr_2]
           when :less_than
-            record_1[name_1] < record_2[name_2]
+            record_1[expr_1] < record_2[expr_2]
           end
 
         result ? 1 : 0
+      end
+
+      def score_datasets(dataset_1, dataset_2)
+        # FIXME: nil value equality
+
+        _score_datasets(dataset_1, dataset_2)
+      end
+
+      def score_dataset(dataset)
+        # FIXME: nil value equality
+
+        expr = @set_1.collect(&:to_expr)
+        if @set_2.collect(&:to_expr) != expr
+          return _score_datasets(dataset, dataset)
+        end
+
+        enum = dataset.order(*expr).to_enum
+        begin
+          record = enum.next
+        rescue StopIteration
+          return
+        end
+        group = [record]
+        last_value = record.values_at(*expr)
+        loop do
+          begin
+            record = enum.next
+          rescue StopIteration
+            break
+          end
+          value = record.values_at(*expr)
+          if value == last_value
+            group << record
+          else
+            score_group(group)
+            group.clear
+            group << record
+            last_value = value
+          end
+        end
+        score_group(group)
+      end
+
+      private
+
+      def _score_datasets(dataset_1, dataset_2)
+        expr_1 = @set_1.collect(&:to_expr)
+        expr_2 = @set_2.collect(&:to_expr)
+        enum_1 = dataset_1.order(*expr_1).to_enum
+        enum_2 = dataset_2.order(*expr_2).to_enum
+
+        begin
+          record_1 = enum_1.next
+          record_2 = enum_2.next
+        rescue StopIteration
+          # no pairs to score
+          return
+        end
+        group_1 = []
+        group_2 = []
+        loop do
+          value_1 = record_1.values_at(*expr_1)
+          value_2 = record_2.values_at(*expr_2)
+          result = value_1 <=> value_2
+          if result == 0
+            last_value = value_1
+            group_1 << record_1
+            group_2 << record_2
+
+            state = :right
+            loop do
+              begin
+                case state
+                when :left
+                  record_1 = enum_1.next
+                  value_1 = record_1.values_at(*expr_1)
+                  result = last_value == value_1
+                when :right
+                  record_2 = enum_2.next
+                  value_2 = record_2.values_at(*expr_2)
+                  result = last_value == value_2
+                end
+              rescue StopIteration
+                result = false
+                case state
+                when :left
+                  record_1 = :eof
+                when :right
+                  record_2 = :eof
+                end
+              end
+
+              if result
+                case state
+                when :left
+                  group_1 << record_1
+                when :right
+                  group_2 << record_2
+                end
+              else
+                case state
+                when :left
+                  # done with this group
+                  score_groups(group_1, group_2)
+                  group_1.clear
+                  group_2.clear
+                  break
+                when :right
+                  state = :left
+                end
+              end
+            end
+            if record_1 == :eof || record_2 == :eof
+              break
+            end
+          else
+            begin
+              if result < 0
+                record_1 = enum_1.next
+              else
+                record_2 = enum_2.next
+              end
+            rescue StopIteration
+              break
+            end
+          end
+        end
+      end
+
+      def score_groups(group_1, group_2)
+        group_1.each do |record_1|
+          group_2.each do |record_2|
+            changed
+            notify_observers(record_1, record_2, 1)
+          end
+        end
+      end
+
+      def score_group(group)
+        (group.length - 1).times do |i|
+          ((i+1)...group.length).each do |j|
+            changed
+            notify_observers(group[i], group[j], 1)
+          end
+        end
       end
     end
 
--- test/unit/comparators/test_compare.rb
+++ test/unit/comparators/test_compare.rb
@@ -14,52 +14,204 @@ class UnitTests::TestCompare < Test::Unit::TestCase
   end
 
   test "score for not equal to" do
-    field_1 = stub('foo field', :name => :foo, :ruby_type => { :type => Integer })
-    field_2 = stub('bar field', :name => :bar, :ruby_type => { :type => Integer })
+    field_1 = stub('foo field', :to_expr => :foo, :ruby_type => { :type => Integer })
+    field_2 = stub('bar field', :to_expr => :bar, :ruby_type => { :type => Integer })
     comp = Compare.new([field_1], [field_2], :not_equal)
+    assert_equal :simple, comp.type
     assert_equal 1, comp.score({:foo => 10}, {:bar => 5})
     assert_equal 0, comp.score({:foo => 5}, {:bar => 5})
     assert_equal 1, comp.score({:foo => 0}, {:bar => 5})
   end
 
   test "score for greater than" do
-    field_1 = stub('foo field', :name => :foo, :ruby_type => { :type => Integer })
-    field_2 = stub('bar field', :name => :bar, :ruby_type => { :type => Integer })
+    field_1 = stub('foo field', :to_expr => :foo, :ruby_type => { :type => Integer })
+    field_2 = stub('bar field', :to_expr => :bar, :ruby_type => { :type => Integer })
     comp = Compare.new([field_1], [field_2], :greater_than)
+    assert_equal :simple, comp.type
     assert_equal 1, comp.score({:foo => 10}, {:bar => 5})
     assert_equal 0, comp.score({:foo => 5}, {:bar => 5})
     assert_equal 0, comp.score({:foo => 0}, {:bar => 5})
   end
 
   test "score for greater than or equal to" do
-    field_1 = stub('foo field', :name => :foo, :ruby_type => { :type => Integer })
-    field_2 = stub('bar field', :name => :bar, :ruby_type => { :type => Integer })
+    field_1 = stub('foo field', :to_expr => :foo, :ruby_type => { :type => Integer })
+    field_2 = stub('bar field', :to_expr => :bar, :ruby_type => { :type => Integer })
     comp = Compare.new([field_1], [field_2], :greater_than_or_equal_to)
+    assert_equal :simple, comp.type
     assert_equal 1, comp.score({:foo => 10}, {:bar => 5})
     assert_equal 1, comp.score({:foo => 5}, {:bar => 5})
     assert_equal 0, comp.score({:foo => 0}, {:bar => 5})
   end
 
   test "score for less than or equal to" do
-    field_1 = stub('foo field', :name => :foo, :ruby_type => { :type => Integer })
-    field_2 = stub('bar field', :name => :bar, :ruby_type => { :type => Integer })
+    field_1 = stub('foo field', :to_expr => :foo, :ruby_type => { :type => Integer })
+    field_2 = stub('bar field', :to_expr => :bar, :ruby_type => { :type => Integer })
     comp = Compare.new([field_1], [field_2], :less_than_or_equal_to)
+    assert_equal :simple, comp.type
     assert_equal 0, comp.score({:foo => 10}, {:bar => 5})
     assert_equal 1, comp.score({:foo => 5}, {:bar => 5})
     assert_equal 1, comp.score({:foo => 0}, {:bar => 5})
   end
 
   test "score for less than" do
-    field_1 = stub('foo field', :name => :foo, :ruby_type => { :type => Integer })
-    field_2 = stub('bar field', :name => :bar, :ruby_type => { :type => Integer })
+    field_1 = stub('foo field', :to_expr => :foo, :ruby_type => { :type => Integer })
+    field_2 = stub('bar field', :to_expr => :bar, :ruby_type => { :type => Integer })
     comp = Compare.new([field_1], [field_2], :less_than)
+    assert_equal :simple, comp.type
     assert_equal 0, comp.score({:foo => 10}, {:bar => 5})
     assert_equal 0, comp.score({:foo => 5}, {:bar => 5})
     assert_equal 1, comp.score({:foo => 0}, {:bar => 5})
   end
 
-  test "score_datasets for equal to" do
-    pend
+  test "score_datasets with one field equality" do
+    field_1 = stub('foo field', :to_expr => :foo, :ruby_type => { :type => Integer })
+    field_2 = stub('bar field', :to_expr => :bar, :ruby_type => { :type => Integer })
+    comp = Compare.new([field_1], [field_2], :equal_to)
+    assert_equal :advanced, comp.type
+    observer = stub('observer', :update => nil)
+    comp.add_observer(observer)
+
+    dataset_1 = stub('dataset 1')
+    dataset_1.expects(:order).with(:foo).returns(dataset_1)
+    dataset_1.expects(:each).multiple_yields(
+      [(record_1_1 = {:id => 1, :foo => 123})],
+      [(record_1_2 = {:id => 2, :foo => 456})],
+      [(record_1_3 = {:id => 3, :foo => 456})]
+    )
+
+    dataset_2 = stub('dataset 2')
+    dataset_2.expects(:order).with(:bar).returns(dataset_2)
+    dataset_2.expects(:each).multiple_yields(
+      [(record_2_1 = {:id => 100, :bar => 123})],
+      [(record_2_2 = {:id => 101, :bar => 456})],
+      [(record_2_3 = {:id => 102, :bar => 456})]
+    )
+
+    observer.expects(:update).with(record_1_1, record_2_1, 1)
+    observer.expects(:update).with(record_1_2, record_2_2, 1)
+    observer.expects(:update).with(record_1_2, record_2_3, 1)
+    observer.expects(:update).with(record_1_3, record_2_2, 1)
+    observer.expects(:update).with(record_1_3, record_2_3, 1)
+
+    comp.score_datasets(dataset_1, dataset_2)
+  end
+
+  test "score_datasets with multiple field equality" do
+    field_1_1 = stub('foo field', :to_expr => :foo, :ruby_type => { :type => Integer })
+    field_1_2 = stub('bar field', :to_expr => :bar, :ruby_type => { :type => Integer })
+    field_2_1 = stub('baz field', :to_expr => :baz, :ruby_type => { :type => Integer })
+    field_2_2 = stub('qux field', :to_expr => :qux, :ruby_type => { :type => Integer })
+    comp = Compare.new([field_1_1, field_1_2], [field_2_1, field_2_2], :equal_to)
+    assert_equal :advanced, comp.type
+    observer = stub('observer', :update => nil)
+    comp.add_observer(observer)
+
+    dataset_1 = stub('dataset 1')
+    dataset_1.expects(:order).with(:foo, :bar).returns(dataset_1)
+    dataset_1.expects(:each).multiple_yields(
+      [(record_1_1 = {:id => 1, :foo => 123, :bar => 123})],
+      [(record_1_2 = {:id => 2, :foo => 123, :bar => 456})],
+      [(record_1_3 = {:id => 3, :foo => 123, :bar => 789})],
+      [(record_1_4 = {:id => 4, :foo => 456, :bar => 123})]
+    )
+
+    dataset_2 = stub('dataset 2')
+    dataset_2.expects(:order).with(:baz, :qux).returns(dataset_2)
+    dataset_2.expects(:each).multiple_yields(
+      [(record_2_1 = {:id => 100, :baz => 123, :qux => 123})],
+      [(record_2_2 = {:id => 101, :baz => 123, :qux => 123})],
+      [(record_2_3 = {:id => 102, :baz => 123, :qux => 789})],
+      [(record_2_4 = {:id => 103, :baz => 456, :qux => 123})],
+      [(record_2_5 = {:id => 104, :baz => 456, :qux => 456})]
+    )
+
+    observer.expects(:update).with(record_1_1, record_2_1, 1)
+    observer.expects(:update).with(record_1_1, record_2_2, 1)
+    observer.expects(:update).with(record_1_3, record_2_3, 1)
+    observer.expects(:update).with(record_1_4, record_2_4, 1)
+
+    comp.score_datasets(dataset_1, dataset_2)
+  end
+
+  test "score_dataset with same single field equality" do
+    field = stub('foo field', :to_expr => :foo, :ruby_type => { :type => Integer })
+    comp = Compare.new([field], [field], :equal_to)
+    assert_equal :advanced, comp.type
+    observer = stub('observer', :update => nil)
+    comp.add_observer(observer)
+
+    dataset = stub('dataset')
+    dataset.expects(:order).with(:foo).returns(dataset)
+    dataset.expects(:each).multiple_yields(
+      [(record_1 = {:id => 1, :foo => 123})],
+      [(record_2 = {:id => 2, :foo => 456})],
+      [(record_3 = {:id => 3, :foo => 456})]
+    )
+
+    observer.expects(:update).with(record_2, record_3, 1)
+    comp.score_dataset(dataset)
+  end
+
+  test "score_dataset with same multiple field equality" do
+    field_1 = stub('foo field', :to_expr => :foo, :ruby_type => { :type => Integer })
+    field_2 = stub('bar field', :to_expr => :bar, :ruby_type => { :type => Integer })
+    comp = Compare.new([field_1, field_2], [field_1, field_2], :equal_to)
+    assert_equal :advanced, comp.type
+    observer = stub('observer', :update => nil)
+    comp.add_observer(observer)
+
+    dataset = stub('dataset')
+    dataset.expects(:order).with(:foo, :bar).returns(dataset)
+    dataset.expects(:each).multiple_yields(
+      [(record_1 = {:id => 1, :foo => 123, :bar => 123})],
+      [(record_2 = {:id => 2, :foo => 123, :bar => 123})],
+      [(record_3 = {:id => 3, :foo => 123, :bar => 123})],
+      [(record_4 = {:id => 4, :foo => 123, :bar => 456})],
+      [(record_5 = {:id => 5, :foo => 456, :bar => 123})],
+      [(record_6 = {:id => 6, :foo => 456, :bar => 456})],
+      [(record_7 = {:id => 7, :foo => 456, :bar => 456})]
+    )
+
+    observer.expects(:update).with(record_1, record_2, 1)
+    observer.expects(:update).with(record_1, record_3, 1)
+    observer.expects(:update).with(record_2, record_3, 1)
+    observer.expects(:update).with(record_6, record_7, 1)
+
+    comp.score_dataset(dataset)
+  end
+
+  test "score_dataset with different single field equality" do
+    field_1 = stub('foo field', :to_expr => :foo, :ruby_type => { :type => Integer })
+    field_2 = stub('bar field', :to_expr => :bar, :ruby_type => { :type => Integer })
+    comp = Compare.new([field_1], [field_2], :equal_to)
+    assert_equal :advanced, comp.type
+    observer = stub('observer', :update => nil)
+    comp.add_observer(observer)
+
+    dataset = stub('dataset')
+    dataset_1 = stub('dataset 1')
+    dataset.expects(:order).with(:foo).returns(dataset_1)
+    dataset_1.expects(:each).multiple_yields(
+      [(record_1 = {:id => 1, :foo => 123, :bar => 456})],
+      [(record_2 = {:id => 2, :foo => 456, :bar => 456})],
+      [(record_3 = {:id => 3, :foo => 456, :bar => 123})]
+    )
+
+    dataset_2 = stub('dataset 2')
+    dataset.expects(:order).with(:bar).returns(dataset_2)
+    dataset_2.expects(:each).multiple_yields(
+      [record_3],
+      [record_1],
+      [record_2]
+    )
+
+    observer.expects(:update).with(record_1, record_3, 1)
+    observer.expects(:update).with(record_2, record_1, 1)
+    observer.expects(:update).with(record_2, record_2, 1)
+    observer.expects(:update).with(record_3, record_1, 1)
+    observer.expects(:update).with(record_3, record_2, 1)
+
+    comp.score_dataset(dataset)
   end
 
   test "registers itself" do
@@ -67,24 +219,24 @@ class UnitTests::TestCompare < Test::Unit::TestCase
   end
 
   test "requires equal size sets" do
-    field_1 = stub('foo field', :name => :foo, :ruby_type => { :type => Integer })
-    field_2 = stub('bar field', :name => :bar, :ruby_type => { :type => Integer })
+    field_1 = stub('foo field', :to_expr => :foo, :ruby_type => { :type => Integer })
+    field_2 = stub('bar field', :to_expr => :bar, :ruby_type => { :type => Integer })
     assert_raises do
       Compare.new([field_1, field_2], [], :greater_than_or_equal_to)
     end
   end
 
   test "requires that sets have values with alike types" do
-    field_1 = stub('foo field', :name => :foo, :ruby_type => { :type => Integer })
-    field_2 = stub('bar field', :name => :bar, :ruby_type => { :type => Date })
+    field_1 = stub('foo field', :to_expr => :foo, :ruby_type => { :type => Integer })
+    field_2 = stub('bar field', :to_expr => :bar, :ruby_type => { :type => Date })
     assert_raises do
       Compare.new([field_1], [field_2], :greater_than_or_equal_to)
     end
   end
 
   test "requires valid operation" do
-    field_1 = stub('foo field', :name => :foo, :ruby_type => { :type => Integer })
-    field_2 = stub('bar field', :name => :bar, :ruby_type => { :type => Integer })
+    field_1 = stub('foo field', :to_expr => :foo, :ruby_type => { :type => Integer })
+    field_2 = stub('bar field', :to_expr => :bar, :ruby_type => { :type => Integer })
     assert_raises do
       Compare.new([field_1], [field_2], 'foo')
     end
