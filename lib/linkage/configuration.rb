9b7724963b6e46171c001f07fa18b04f73d4f677
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2014-01-02 15:09:09 -0600

    Configuration tweaks
    - add default value for dataset_2 in #initialize
    - rename comparisons to comparators

--- lib/linkage/configuration.rb
+++ lib/linkage/configuration.rb
@@ -1,12 +1,12 @@
 module Linkage
   class Configuration
-    attr_reader :dataset_1, :dataset_2, :comparisons
+    attr_reader :dataset_1, :dataset_2, :comparators
     attr_accessor :record_cache_size
 
-    def initialize(dataset_1, dataset_2)
+    def initialize(dataset_1, dataset_2 = nil)
       @dataset_1 = dataset_1
       @dataset_2 = dataset_2
-      @comparisons = []
+      @comparators = []
       @record_cache_size = 10_000
     end
 
@@ -15,7 +15,7 @@ module Linkage
       if klass.nil?
         raise "unknown comparator: #{name}"
       end
-      @comparisons << klass.new(*args, &block)
+      @comparators << klass.new(*args, &block)
     end
   end
 end
--- test/unit/test_configuration.rb
+++ test/unit/test_configuration.rb
@@ -1,7 +1,7 @@
 require 'helper'
 
 class UnitTests::TestConfiguration < Test::Unit::TestCase
-  test "adding comparison" do
+  test "adding comparator" do
     dataset_1 = stub('dataset 1')
     dataset_2 = stub('dataset 2')
     config = Linkage::Configuration.new(dataset_1, dataset_2)
@@ -9,6 +9,6 @@ class UnitTests::TestConfiguration < Test::Unit::TestCase
     compare = stub('compare')
     Linkage::Comparators::Compare.expects(:new).with([:foo], [:foo], :equal_to).returns(compare)
     config.compare([:foo], [:foo], :equal_to)
-    assert_equal compare, config.comparisons[0]
+    assert_equal compare, config.comparators[0]
   end
 end
