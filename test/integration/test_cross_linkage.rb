2cab71c133fac947c2f25a2e9a5a7bd86afc1a07
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2014-01-29 13:18:30 -0600

    Update cross linkage integration test

--- test/integration/test_cross_linkage.rb
+++ test/integration/test_cross_linkage.rb
@@ -15,7 +15,7 @@ module IntegrationTests
       FileUtils.remove_entry_secure(@tmpdir)
     end
 
-    test "one mandatory field equality on single threaded runner" do
+    test "one field equality on single threaded runner" do
       # insert the test data
       database do |db|
         db.create_table(:foo) { primary_key(:id); Integer(:foo); Integer(:bar) }
@@ -25,25 +25,35 @@ module IntegrationTests
 
       ds = Linkage::Dataset.new(@tmpuri, "foo", :single_threaded => true)
 
-      tmpuri = @tmpuri
-      conf = ds.link_with(ds) do
-        lhs[:foo].must == rhs[:bar]
-        save_results_in(tmpuri, :single_threaded => true)
+      score_file = File.join(@tmpdir, 'scores.csv')
+      score_set = Linkage::ScoreSet['csv'].new(score_file)
+      match_file = File.join(@tmpdir, 'matches.csv')
+      match_set = Linkage::MatchSet['csv'].new(match_file)
+      conf = ds.link_with(ds, score_set, match_set) do |conf|
+        conf.compare([:foo], [:bar], :equal_to)
+        conf.algorithm = :mean
+        conf.threshold = 1
       end
-      assert_equal :cross, conf.linkage_type
+
       runner = Linkage::SingleThreadedRunner.new(conf)
       runner.execute
 
-      database do |db|
-        assert_equal 5, db[:groups].count, PP.pp(db[:groups].all, "")
-        db[:groups].order(:foo_bar).each_with_index do |row, i|
-          assert_equal i, row[:foo_bar]
-        end
+      score_csv = CSV.read(score_file, :headers => true)
+      assert_equal 1000, score_csv.length
+      score_csv.each do |row|
+        id_1 = row['id_1'].to_i
+        id_2 = row['id_2'].to_i
+        assert (id_1 % 10) == (id_2 % 5)
+        assert_equal "1", row['score']
+      end
 
-        assert_equal 1000, db[:matches].count
-        db[:matches].order(:record_1_id, :record_2_id).each do |row|
-          assert_equal row[:record_1_id] % 10, row[:record_2_id] % 5
-        end
+      match_csv = CSV.read(match_file, :headers => true)
+      assert_equal 1000, match_csv.length
+      match_csv.each do |row|
+        id_1 = row['id_1'].to_i
+        id_2 = row['id_2'].to_i
+        assert (id_1 % 10) == (id_2 % 5)
+        assert_equal "1", row['score']
       end
     end
 
@@ -54,19 +64,37 @@ module IntegrationTests
           Array.new(100) { |i| [i, i % 10, i % 20] })
       end
 
+      score_file = File.join(@tmpdir, 'scores.csv')
+      score_set = Linkage::ScoreSet['csv'].new(score_file)
+      match_file = File.join(@tmpdir, 'matches.csv')
+      match_set = Linkage::MatchSet['csv'].new(match_file)
+
       ds = Linkage::Dataset.new(@tmpuri, "foo", :single_threaded => true)
-      tmpuri = @tmpuri
-      conf = ds.link_with(ds) do
-        lhs[:foo].must == rhs[:foo]
-        lhs[:bar].must == 0
-        rhs[:bar].must == 10
-        save_results_in(tmpuri)
+      ds_1 = ds.filter(:bar => 0)
+      ds_2 = ds.filter(:bar => 10)
+      conf = ds_1.link_with(ds_2, score_set, match_set) do |conf|
+        conf.compare([:foo], [:foo], :equal_to)
+        conf.algorithm = :mean
+        conf.threshold = 1
       end
+
       runner = Linkage::SingleThreadedRunner.new(conf)
       runner.execute
 
-      database do |db|
-        assert_equal 1, db[:groups].count
+      score_csv = CSV.read(score_file, :headers => true)
+      assert_equal 25, score_csv.length
+      score_csv.each do |row|
+        id_1 = row['id_1'].to_i
+        id_2 = row['id_2'].to_i
+        assert (id_1 % 10) == (id_1 % 10)
+      end
+
+      match_csv = CSV.read(match_file, :headers => true)
+      assert_equal 25, match_csv.length
+      match_csv.each do |row|
+        id_1 = row['id_1'].to_i
+        id_2 = row['id_2'].to_i
+        assert (id_1 % 10) == (id_1 % 10)
       end
     end
   end
