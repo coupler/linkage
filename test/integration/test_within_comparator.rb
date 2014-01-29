a75d68ff0d62fb98e72d473d3d1a8ffd3c899614
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2014-01-29 13:32:36 -0600

    Update within comparator integration test

--- test/integration/test_within_comparator.rb
+++ test/integration/test_within_comparator.rb
@@ -10,26 +10,27 @@ module IntegrationTests
         db[:bar].import([:id, :num], (1..10).collect { |i| [i, i] })
       end
 
-      # result set
-      csv_file = Tempfile.new('linkage')
-      csv_file.close
-      score_set = Linkage::ScoreSet['csv'].new(csv_file.path)
+      score_file = Tempfile.new('linkage')
+      score_file.close
+      score_set = Linkage::ScoreSet['csv'].new(score_file.path)
+      match_file = Tempfile.new('linkage')
+      match_file.close
+      match_set = Linkage::MatchSet['csv'].new(match_file.path)
 
       # config
       db_opts = database_options_for('sqlite')
       dataset_1 = Linkage::Dataset.new(db_opts, "foo")
       dataset_2 = Linkage::Dataset.new(db_opts, "bar")
-      conf = dataset_1.link_with(dataset_2, score_set) do |conf|
+      conf = dataset_1.link_with(dataset_2, score_set, match_set) do |conf|
         conf.within(:num, :num, 5)
       end
 
       runner = Linkage::SingleThreadedRunner.new(conf)
       runner.execute
-      score_set.close
 
-      csv = CSV.read(csv_file.path, :headers => true)
-      assert_equal 100, csv.length
-      csv.each do |row|
+      score_csv = CSV.read(score_file.path, :headers => true)
+      assert_equal 100, score_csv.length
+      score_csv.each do |row|
         assert_equal "1", row['comparator']
         # ids same as values
         id_1 = row['id_1'].to_i
@@ -38,39 +39,6 @@ module IntegrationTests
           assert_equal 1, row['score'].to_i, row
         else
           assert_equal 0, row['score'].to_i
-        end
-      end
-    end
-
-    test "within comparator with functions" do
-      pend
-      database_for('sqlite') do |db|
-        db.create_table(:foo) { primary_key(:id); Integer(:num); String(:parity) }
-        db.create_table(:bar) { primary_key(:id); Integer(:num); String(:parity)  }
-        db[:foo].import([:id, :num, :parity], (1..10).collect { |i| [i, i, i % 2 == 0 ? "even" : "odd"] })
-        db[:bar].import([:id, :num, :parity], (1..10).collect { |i| [i, i, i % 2 == 0 ? "even" : "odd"] })
-      end
-
-      db_opts = database_options_for('sqlite')
-      dataset_1 = Linkage::Dataset.new(db_opts, "foo")
-      dataset_2 = Linkage::Dataset.new(db_opts, "bar")
-      conf = dataset_1.link_with(dataset_2) do
-        lhs[:parity].must == rhs[:parity]
-        cast(lhs[:num], 'integer').must be_within(5).of(cast(rhs[:num], 'integer'))
-        save_results_in(db_opts)
-      end
-
-      runner = Linkage::SingleThreadedRunner.new(conf)
-      runner.execute
-
-      database_for('sqlite') do |db|
-        assert_equal db[:scores].count, 50
-        db[:scores].order(:record_1_id, :record_2_id).each do |score|
-          if (score[:record_2_id] - score[:record_1_id]).abs <= 5
-            assert_equal 1, score[:score]
-          else
-            assert_equal 0, score[:score]
-          end
         end
       end
     end
