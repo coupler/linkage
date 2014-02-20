52027685be589b6a2ae67fe68171a881158a1dd8
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2014-02-20 10:57:48 -0600

    Update Comparator documentation

--- lib/linkage/comparator.rb
+++ lib/linkage/comparator.rb
@@ -1,36 +1,57 @@
 module Linkage
-  # @abstract Abstract class to represent record comparators.
+  # Comparator is the superclass for comparators in Linkage. Comparators are
+  # used
+  # @abstract
   class Comparator
     include Observable
 
-    # Register a new comparator.
-    #
-    # @param [Class] klass Comparator subclass
-    def self.register(name, klass)
-      methods = klass.instance_methods(false)
-      if !methods.include?(:score) && (!methods.include?(:score_datasets) || !methods.include?(:score_dataset))
-        raise ArgumentError, "class must define either #score or both #score_datasets and #score_dataset methods"
+    class << self
+      # Register a new comparator.
+      #
+      # @param [String] name Comparator name used in {Comparator.klass_for}
+      # @param [Class] klass Comparator subclass
+      def register(name, klass)
+        methods = klass.instance_methods(false)
+        if !methods.include?(:score) && (!methods.include?(:score_datasets) || !methods.include?(:score_dataset))
+          raise ArgumentError, "class must define either #score or both #score_datasets and #score_dataset methods"
+        end
+
+        @comparators ||= {}
+        @comparators[name] = klass
       end
 
-      @comparators ||= {}
-      @comparators[name] = klass
+      # Return a registered Comparator subclass or nil if it doesn't exist.
+      #
+      # @param [String] name of registered comparator
+      # @return [Class, nil]
+      def klass_for(name)
+        @comparators ? @comparators[name] : nil
+      end
+      alias :[] :klass_for
     end
 
-    def self.[](name)
-      @comparators ? @comparators[name] : nil
-    end
-
+    # Return the type of this comparator.
+    #
+    # @return [Symbol] either `:simple` or `:advanced`
     def type
       @type || :simple
     end
 
-    # @abstract Override this to return the score of the linkage strength of
-    #   two records.
-    # @return [Numeric]
+    # Override this to return the score of the linkage strength of two records.
+    #
+    # @abstract
+    # @param [Hash] record_1 data from first record
+    # @param [Hash] record_2 data from second record
+    # @return [Numeric] value between 0 and 1 (inclusive)
     def score(record_1, record_2)
       raise NotImplementedError
     end
 
+    # Calls {#score} with two hashes of record data. The result is then used to
+    # notify any observers.
+    #
+    # @param [Hash] record_1 data from first record
+    # @param [Hash] record_2 data from second record
     def score_and_notify(record_1, record_2)
       value = score(record_1, record_2)
       changed
