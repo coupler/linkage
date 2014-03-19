2abcc829b0b8fc15a3df9832a91721ea6d9af2ec
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2014-03-19 13:00:40 -0500

    Add ResultSet documentation

--- lib/linkage/result_set.rb
+++ lib/linkage/result_set.rb
@@ -1,4 +1,25 @@
 module Linkage
+  # A {ResultSet} contains a {ScoreSet} and a {MatchSet}. To understand the
+  # purpose of a {ResultSet}, it helps to understand the recording process
+  # first.
+  #
+  # During a record linkage, one or more {Comparator}s generate scores. Each
+  # score is recorded by a {ScoreRecorder}, which uses a {ScoreSet} to actually
+  # save the score. After the scoring is complete, a {Matcher} combines the
+  # scores to create matches. Each match is recorded by a {MatchRecorder}, which
+  # uses a {MatchSet} to actually save the score.
+  #
+  # So to save scores and matches, we need both a {ScoreSet} and a {MatchSet}.
+  # To make this easier, a {ResultSet} configures both.
+  #
+  # {ResultSet} is the superclass of implementations for different formats.
+  # Currently there are two formats for storing scores and matches:
+  #
+  # * CSV ({ResultSets::CSV})
+  # * Database ({ResultSets::Database})
+  #
+  # See the documentation for result set you're interested in for more
+  # information.
   class ResultSet
     # Register a result set.
     #
