fab55e2bcd055d3bcb53534cc9d28aa4c3303b7c
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2014-03-19 15:33:05 -0500

    Add documentation for ResultSets::CSV

--- lib/linkage/result_sets/csv.rb
+++ lib/linkage/result_sets/csv.rb
@@ -1,5 +1,47 @@
 module Linkage
   module ResultSets
+    # {CSV ResultSets::CSV} is the {ResultSet ResultSet} for writing to CSV
+    # files. You can use it by either referencing it directly like so:
+    #
+    # ```ruby
+    # result_set = Linkage::ResultSets::CSV.new(my_options)
+    # ```
+    #
+    # Or by using {ResultSet.[] ResultSet.[]}:
+    #
+    # ```ruby
+    # result_set = Linkage::ResultSet['csv'].new(my_options)
+    # ```
+    #
+    # There is a slight difference between these two ways, however. The latter
+    # looks up the {ResultSet} class that is registered under the name `'csv'`.
+    # By default, the registered class is {CSV Linkage::ResultSets::CSV}, but if
+    # that gets overridden (by a plugin, for example), the result of
+    # `Linkage::ResultSet['csv']` will be whatever was registered.
+    #
+    # {CSV#initialize ResultSets::CSV.new} takes either a directory name or a
+    # Hash of options.  If you use a directory name, it will set up a
+    # {ScoreSets::CSV} and {MatchSets::CSV} to use files within the directory
+    # you specified. For example:
+    #
+    # ```ruby
+    # result_set = Linkage::ResultSet['csv'].new("/some/path")
+    # ```
+    #
+    # This will create the following files:
+    #
+    # * `/some/path/scores.csv`  (for scores)
+    # * `/some/path/matches.csv` (for matches)
+    #
+    # If you use a Hash of options, you can choose each filename individually
+    # like so:
+    #
+    # ```ruby
+    # result_set = Linkage::ResultSet['csv'].new({
+    #   :scores  => { :filename => "/some/path/foo-scores.csv" },
+    #   :matches => { :filename => "/some/other/path/foo-matches.csv" }
+    # })
+    # ```
     class CSV < ResultSet
       def initialize(dir_or_options = nil)
         opts =
