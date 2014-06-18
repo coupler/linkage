a789a17c1259167a262e60608bf8cce0140e51e2
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2014-06-18 14:12:40 -0500

    Add documentation for Linkage exceptions

--- lib/linkage/exceptions.rb
+++ lib/linkage/exceptions.rb
@@ -1,5 +1,10 @@
 module Linkage
+  # Generic error.
   class Error < Exception; end
+
+  # Error raised when a file would be overwritten.
   class ExistsError < Error; end
+
+  # Error raised when trying to read a file that doesn't exist.
   class MissingError < Error; end
 end
